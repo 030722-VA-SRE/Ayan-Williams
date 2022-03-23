2380503ea47f859f88dbcd51add026e3be94ab13
Ayan <ayan396@revature.net>
2022-03-22 22:48:04 -0500

    MVP DONE \o/ git add .!

---
+++ Bugg/src/main/java/com/revature/BuggTest.java
@@ -0,0 +1,16 @@
+package com.revature;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+// for importing in sts shortcut is ctrl+shift+o
+import org.junit.jupiter.api.Test;
+
+
+public class BuggTest {
+
+}
